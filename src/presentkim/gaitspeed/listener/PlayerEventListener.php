d62dee05bb0280c5bca7fc7368d3794362d5c0a3
PresentKim <qjrmckd@gmail.com>
2018-01-05 08:11:57 +0900

    Add player move listener

--- src/presentkim/gaitspeed/listener/PlayerEventListener.php
+++ src/presentkim/gaitspeed/listener/PlayerEventListener.php
@@ -3,7 +3,7 @@
 namespace presentkim\gaitspeed\listener;
 
 use pocketmine\event\{
-  Listener, player\PlayerJoinEvent, player\PlayerRespawnEvent
+  Listener, player\PlayerJoinEvent, player\PlayerMoveEvent, player\PlayerRespawnEvent
 };
 use presentkim\gaitspeed\GaitSpeedMain as Plugin;
 
@@ -11,6 +11,14 @@ class PlayerEventListener implements Listener{
 
     /** @var Plugin */
     private $owner = null;
+
+    /**
+     * Array for apply speed after PlayerRespawnEvent
+     * Because PMMP remove effect after PlayerRespawnEvent called....
+     *
+     * @var \pocketmine\item\Item[] array[string => \pocketmine\item\Item]
+     */
+    private $apply = [];
 
     public function __construct(){
         $this->owner = Plugin::getInstance();
@@ -23,6 +31,16 @@ class PlayerEventListener implements Listener{
 
     /** @param PlayerRespawnEvent $event */
     public function onPlayerRespawnEvent(PlayerRespawnEvent $event) : void{
-        $this->owner->applyTo($event->getPlayer());
+        $this->apply[$event->getPlayer()->getName()] = true;
+    }
+
+    /** @param PlayerMoveEvent $event */
+    public function onPlayerMoveEvent(PlayerMoveEvent $event) : void{
+        $player = $event->getPlayer();
+        $playerName = $player->getName();
+        if (isset($this->apply[$playerName])) {
+            $this->owner->applyTo($player);
+            unset($this->apply[$playerName]);
+        }
     }
 }
