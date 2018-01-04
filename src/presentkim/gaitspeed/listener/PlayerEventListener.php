0c40504e9a6b8714fb4e7dd51a77b546de2445dc
PresentKim <qjrmckd@gmail.com>
2018-01-05 06:46:41 +0900

    Set default speed at player respawn

--- src/presentkim/gaitspeed/listener/PlayerEventListener.php
+++ src/presentkim/gaitspeed/listener/PlayerEventListener.php
@@ -21,8 +21,11 @@ class PlayerEventListener implements Listener{
     public function onPlayerRespawnEvent(PlayerRespawnEvent $event) : void{
         $player = $event->getPlayer();
         $result = $this->owner->query('SELECT gait_speed FROM gait_speed_list WHERE player_name = ' . strtolower($player->getName()) . ';')->fetchArray(SQLITE3_NUM)[0];
-        if ($result) { // When query result is exists
-            $player->getAttributeMap()->getAttribute(Attribute::MOVEMENT_SPEED)->setValue(((int) $result) * 0.001);
+        if ($result !== null) { // When query result is exists
+            $speed = ((int) $result) * 0.001;
+        } else {
+            $speed = ((int) $this->owner->getConfig()->get("default-speed")) * 0.001;
         }
+        $player->getAttributeMap()->getAttribute(Attribute::MOVEMENT_SPEED)->setValue($speed);
     }
 }
